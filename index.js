8cbf60703f0a441608209609afcc757b57394b3c
Maxi0001 <63360632+Maxi0001@users.noreply.github.com>
2020-12-01 18:48:44 -0500

    Files

---
+++ index.js
@@ -0,0 +1,20 @@
+/*!
+ * Copyright(c) 2020 JS
+ * MIT Licensed
+ */
+
+/*
+ * Require Modules
+ */
+let hashString = require('./lib/functions/hashString');
+let reloadFile = require('./lib/functions/reloadFile');
+let randomString = require('./lib/functions/randomString');
+
+/**
+ * Module Exports
+ */
+module.exports = {
+    hashString: hashString,
+    reloadFile: reloadFile,
+    randomString: randomString
+}
